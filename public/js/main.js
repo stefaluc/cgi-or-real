8a141a9e9841acab37e09f8cf24718d5d115a486
Lucas Stefanski <stefaluc000@gmail.com>
2016-09-20 23:47:41 -0500

    Images cannot be seen more than once in one game

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,52 @@
-var images = [
-	{src: 'images/00.jpg', answer: 'real'},
-	{src: 'images/01.jpg', answer: 'cgi'}
+var imageDisplay = [
+	{src: 'images/00.jpg', answer: 'cgi'},
+	{src: 'images/01.jpg', answer: 'cgi'},
+	{src: 'images/02.jpg', answer: 'cgi'},
+	{src: 'images/03.jpg', answer: 'cgi'},
+	{src: 'images/04.jpg', answer: 'cgi'},
+	{src: 'images/05.jpg', answer: 'real'}
 ];
+
+function shuffle(array) {
+	let counter = array.length;
+	// While there are elements in the array
+	while (counter > 0) {
+		// Pick a random index
+		let index = Math.floor(Math.random() * counter);
+		// Decrease counter by 1
+		counter--;
+		// And swap the last element with it
+		let temp = array[counter];
+		array[counter] = array[index];
+		array[index] = temp;
+	}
+	return array;
+}
 
 var container = document.getElementById('container');
 
 var Main = React.createClass({
 	getInitialState: function() {
+		var temp = [];
+		for(var i = 0; i < imageDisplay.length; i++) {
+			temp.push(imageDisplay[i]);
+		}
 		return {
 			progression: 0, 
-			score: 0
+			score: 0,
+			images: temp
 		};
 	},
 
 	resetProgression: function() {
+		var temp = [];
+		for(var i = 0; i < imageDisplay.length; i++) {
+			temp.push(imageDisplay[i]);
+		}
 		this.setState({
 			progression: 0,
-			score: 0
+			score: 0,
+			images: temp
 		});
 	},
 
@@ -32,9 +62,10 @@ var Main = React.createClass({
  			score: this.state.score + 1
 		});
 	},
-
+	
 	getImage: function() {
-		return images[Math.floor((Math.random() * 2))];
+		shuffle(this.state.images);
+		return this.state.images.pop();
 	},
 
 	render: function() {
@@ -44,6 +75,8 @@ var Main = React.createClass({
 
 		// starting screen
 		if(state === 0) {
+			console.log(this.state.images);
+			console.log(imageDisplay);
 			components = (
 				<div>
 					<h1>Welcome!</h1>
@@ -52,7 +85,7 @@ var Main = React.createClass({
 			);
 		}
 		// game screen
-		else if(state > 0 && state < 11) {
+		else if(state > 0 && state < 7) {
 			console.log('Progression: ' + this.state.progression);
 			console.log('Score: ' + this.state.score);
 			var image = this.getImage();
@@ -78,6 +111,7 @@ var Main = React.createClass({
 		else {
 			components = (
 				<div>
+					<h1>You correctly guessed {score} out of 10!</h1>
 					<button onClick={this.resetProgression}>Restart</button>
 				</div>
 			);
